274ed8345ae28952ee4de3653f7089e01f9039ee
neiyamilena1 <neiya.dslv@eduge.ch>
2023-12-18 14:54:15 +0100

    finalisation

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -19,18 +19,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -46,6 +55,7 @@ import java.text.NumberFormat
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 
 class MainActivity : ComponentActivity() {
@@ -67,15 +77,20 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TipTimeLayout() {
     var amountInput by remember { mutableStateOf("") }
+    var percentageInput by remember { mutableStateOf("") }
+    var roundUp by remember { mutableStateOf(false) }
+
 
     val amount = amountInput.toDoubleOrNull() ?: 0.0
-    val tip = calculateTip(amount)
+    val percentage = percentageInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount, percentage, roundUp)
 
     Column(
         modifier = Modifier
             .statusBarsPadding()
             .padding(horizontal = 40.dp)
-            .safeDrawingPadding(),
+            .safeDrawingPadding()
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
@@ -85,7 +100,11 @@ fun TipTimeLayout() {
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(alignment = Alignment.Start)
         )
-        EditNumberFieldAmount(value = amountInput, onValueChange = { amountInput = it },modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
+        EditNumberFieldAmount(label = R.string.bill_amount, leadingIcon = R.drawable.money,value = amountInput, onValueChange = { amountInput = it },modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
+        Spacer(modifier = Modifier.height(40.dp))
+        EditNumberFieldPercentage(label = R.string.tip_percentage, leadingIcon = R.drawable.percent, value = percentageInput, onValueChange = { percentageInput = it },modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
+        Spacer(modifier = Modifier.height(20.dp))
+        RoundTheTipRow(roundUp = roundUp, onRoundUpChanged = { roundUp = it },modifier = Modifier.padding(bottom = 32.dp) )
         Spacer(modifier = Modifier.height(40.dp))
         Text(
             text = stringResource(R.string.tip_amount, tip),
@@ -95,12 +114,27 @@ fun TipTimeLayout() {
     }
 }
 @Composable
-fun EditNumberFieldAmount(value: String, onValueChange: (String) -> Unit,modifier: Modifier = Modifier) {
+fun EditNumberFieldAmount(@StringRes label: Int, @DrawableRes leadingIcon: Int, value: String, onValueChange: (String) -> Unit, modifier: Modifier = Modifier) {
     TextField(
         value = value,
+        leadingIcon = { Icon(painter = painterResource(id = leadingIcon), null) },
         onValueChange = onValueChange,
         singleLine = true,
         label = { Text(stringResource(R.string.bill_amount)) },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = modifier
+    )
+}
+
+
+@Composable
+fun EditNumberFieldPercentage(@StringRes label: Int, @DrawableRes leadingIcon: Int, value: String, onValueChange: (String) -> Unit,modifier: Modifier = Modifier) {
+    TextField(
+        value = value,
+        leadingIcon = { Icon(painter = painterResource(id = leadingIcon), null) },
+        onValueChange = onValueChange,
+        singleLine = true,
+        label = { Text(stringResource(R.string.tip_percentage)) },
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
         modifier = modifier
     )
@@ -110,9 +144,32 @@ fun EditNumberFieldAmount(value: String, onValueChange: (String) -> Unit,modifie
  * according to the local currency.
  * Example would be "$10.00".
  */
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
-    val tip = tipPercent / 100 * amount
+private fun calculateTip(amount: Double, tipPercent: Double, roundUp: Boolean): String {
+    var tip = tipPercent / 100 * amount
+    if (roundUp){
+        tip = kotlin.math.ceil(tip)
+    }
     return NumberFormat.getCurrencyInstance().format(tip)
+}
+
+
+@Composable
+fun RoundTheTipRow(roundUp: Boolean, onRoundUpChanged: (Boolean) -> Unit, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .size(48.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = stringResource(R.string.round_up_tip))
+        Switch(
+            modifier = modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.End),
+            checked = roundUp,
+            onCheckedChange = onRoundUpChanged,
+        )
+    }
 }
 
 @Preview(showBackground = true)
